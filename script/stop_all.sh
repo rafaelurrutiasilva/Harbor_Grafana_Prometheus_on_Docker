7316b779d01bd01cc42178725190cfea3030dd1d
Rafael Urrutia Silva <84277015+rafaelurrutiasilva@users.noreply.github.com>
2023-12-13 14:16:15 +0100

    Update stop_all.sh

--- script/stop_all.sh
+++ script/stop_all.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+HARBOR_COMPOSE_FILE=~/harbor/docker-compose.yml
+CONTAINERS="prometheus node-exporter grafana"
 
+echo "Stopping all Containers"
+docker-compose -f $HARBOR_COMPOSE_FILE down
+docker stop $CONTAINERS
+docker rm $CONTAINERS
